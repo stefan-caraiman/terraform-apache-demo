4a1421ec9440837502df66f80458cc52222f9fbe
Stefan Caraiman <stefan.caraiman@endava.com>
2020-02-16 11:22:21 +0200

    Update userdata apache script

--- files/apache.sh
+++ files/apache.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-echo "<h1>Terraform is awesome!</h1>" | sudo tee /var/www/html/index.html
+echo '<h1>Terraform is awesome!</h1><img src="https://i.pinimg.com/736x/42/e5/58/42e558004e65710ce268996ff12a8310.jpg">' | sudo tee /var/www/html/index.html
